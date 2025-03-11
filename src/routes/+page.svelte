731ee0e14cd4c8608c84cff32104cd4accd207e7
onemanteam91 <onemanteam91@gmail.com>
2025-03-11 00:37:00 +0000

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { languageTag } from "$lib/paraglide/runtime";
+  import type { PageData } from "./$types";
+
+  interface Props {
+    data: PageData
+  }
+
+  const {data}:Props = $props()
+
+    console.log('component init tag:', languageTag())
+</script>
+<pre>1. Run `npm run build && node build`</pre>
+<pre>2. Go directly to http://YOUR_DOMAIN:YOUR_PORT/ro</pre>
+<pre>3. Tag from load function should be 'ro', not the default language 'en'</pre>
+<pre>4. You might be required to delete server.allowedHosts from vite.config.ts for this to work (this was requirement specific to the dev environment of the author)</pre>
+<pre>Results:</pre>
+<pre>tagFromLoadFunction: <strong>{data.tagFromLoad}</strong></pre>
+<pre>tagFromSvelteComponentInitialization: <strong>{languageTag()}</strong></pre>
---
+++ src/routes/+page.ts
@@ -0,0 +1,12 @@
+import { languageTag } from "$lib/paraglide/runtime";
+import type { PageLoad } from "./$types";
+
+export const load: PageLoad = async ({ depends }) => {
+  depends("paraglide:lang");
+
+  console.log("load function", languageTag());
+
+  return {
+    tagFromLoad: languageTag(),
+  };
+};
